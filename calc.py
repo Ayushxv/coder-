b47a2b4db7f31ded1444e6ef3d11b94ff37e1bff
Ayush Rajput <88504815+Ayushxv@users.noreply.github.com>
2021-08-09 09:21:12 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first no. :"))
+b = int(input("Enter second no. :"))
+c = int(input("Enter choice :"))
+if(c==1):
+    print(a+b)
+elif(c==2):
+    print(a-b)
+elif(c==3):
+    print(a*b)
+elif(c==4):
+    print(a/b)
+else:
+    print("ERROR")
